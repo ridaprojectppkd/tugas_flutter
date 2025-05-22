d96a1e30f7017a304e798980da2cf1a07d6dbe03
ridaprojectppkd <ridaprojectppkd@gmail.com>
2025-05-22 14:38:22 +0700

    gpttugas4

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-         home: SewaListTile(),
+         home: gptbebe(),
     );
   }
 }
